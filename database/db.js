351feb1e8354619455513856478627be96629a76
Isabella Berekdar <isabellaberekdar@gmail.com>
2020-01-25 18:54:14 -0500

    Changed port for heroku deployment

--- database/db.js
+++ database/db.js
@@ -2,7 +2,9 @@ const Sequelize = require("sequelize");
 const databaseName = "authorize-backend";
 
 console.log("Opening database connection");
+const PORT = process.env.PORT || 5432; /* NEW ADDITION */
 
-const db = new Sequelize(`postgres://localhost:5432/${databaseName}`, { logging: false });
+
+const db = new Sequelize(`postgres://localhost:${PORT}/${databaseName}`, { logging: false });
 
 module.exports = db;
