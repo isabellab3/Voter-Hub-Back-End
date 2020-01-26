c7719d2f05323e6a2488820617d9eb9750fd4e5b
Isabella Berekdar <isabellaberekdar@gmail.com>
2020-01-25 19:24:31 -0500

    deployment

--- database/db.js
+++ database/db.js
@@ -4,11 +4,13 @@ const databaseName = "authorize-backend";
 console.log("Opening database connection");
 const PORT = process.env.PORT || 5432; /* NEW ADDITION */
 
-let db = new Sequelize(`postgres://localhost:5432/${databaseName}`,  { logging: false });
+let db
 
 if (process.env.DATABASE_URL) {
   db = new Sequelize(process.env.DATABASE_URL, { logging: false });
 }
-
+ else {
+  db = new Sequelize(`postgres://localhost:5432/${databaseName}`,  { logging: false });
+ }
 
 module.exports = db;
