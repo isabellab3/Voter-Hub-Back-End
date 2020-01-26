150c56d347ac691ec18d42ee676125fc5a76b5f0
Isabella Berekdar <isabellaberekdar@gmail.com>
2020-01-25 20:30:04 -0500

    Changed cors origin to deployed site

--- app.js
+++ app.js
@@ -46,16 +46,7 @@ const configureApp = () => {
 
   app.use(cors({ credentials: true, origin: 'https://voterhub.netlify.com'}))
 
-  /*
-  
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
-  });
-
-  */
+ 
 
   app.use(
     session({
