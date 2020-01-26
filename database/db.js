58c049594e3ae1ae70eb37a6ce08eee219667bdf
Isabella Berekdar <isabellaberekdar@gmail.com>
2020-01-25 19:22:57 -0500

    fixed bug where const variable was reassigned

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ const databaseName = "authorize-backend";
 console.log("Opening database connection");
 const PORT = process.env.PORT || 5432; /* NEW ADDITION */
 
-const db = new Sequelize(`postgres://localhost:5432/${databaseName}`,  { logging: false });
+let db = new Sequelize(`postgres://localhost:5432/${databaseName}`,  { logging: false });
 
 if (process.env.DATABASE_URL) {
   db = new Sequelize(process.env.DATABASE_URL, { logging: false });
